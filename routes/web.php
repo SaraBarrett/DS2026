544b96413e12a6a19c9028e0f933d0f58f52fc4e
SaraBarrett <sara.c.mon@gmail.com>
2026-06-18 13:49:08 +0100

    routes

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,22 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('homepage');
 });
+
+Route::get('/hello', function () {
+    return '<h5>Olá mundo!</h5>';
+})->name('hello');
+
+Route::get('/curso/{nome}', function ($nome) {
+    return "<h5>Curso : $nome</h5>";
+});
+
+
+
+
+//rota de tratamento de erros, caso entre numa rota não existente
+Route::fallback(function(){
+    return '<p>Está perdido? Não se preocupe que estamos aqui!</p>';
+});
+
