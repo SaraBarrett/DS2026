c17d75105949dd8815b2035558b72425106d41a2
SaraBarrett <sara.c.mon@gmail.com>
2026-06-25 17:16:05 +0100

    database

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
@@ -13,9 +14,11 @@ class UserController extends Controller
 
 public function allUsers(){
     $users = $this->getContacts();
-    //dd($users);
-    
-    return view('users.allUsers',compact('users'));
+
+    $usersFromDb = $this->getUsersFromDb();
+
+
+    return view('users.allUsers',compact('users', 'usersFromDb'));
 }
 
 //função que apenas retorna dados para dentro da class UserController
@@ -28,5 +31,15 @@ protected function getContacts(){
         ['id' =>3, 'name' =>'Mário', 'phone'=>'915444444'],        ['id' =>4, 'name' =>'Hélder', 'phone'=>'915444444'],
     ];
     return  $contacts;
+}
+
+protected function getUsersFromDb(){
+    $usersFromDb = DB::table('users')
+    ->where('email', 'sara@gmail.com')
+    ->select('name', 'id', 'nif', 'address', 'email')
+    ->get();
+
+    return $usersFromDb;
+
 }
 }
--- resources/views/users/allUsers.blade.php
+++ resources/views/users/allUsers.blade.php
@@ -6,4 +6,30 @@
             <li>{{ $user['id'] }} : {{ $user['name'] }} - {{ $user['phone'] }}</li>
         @endforeach
     </ul>
+
+    <h5>Users vindos da base de dados</h5>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Email</th>
+                <th scope="col">Morada</th>
+                <th scope="col">Nif</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($usersFromDb as $user)
+                <tr>
+                    <th scope="row">{{$user->id}}</th>
+                    <td>{{$user->name}}</td>
+                    <td>{{$user->email}}</td>
+                    <td>{{$user->address}}</td>
+                    <td>{{$user->nif}}</td>
+                </tr>
+            @endforeach
+
+
+        </tbody>
+    </table>
 @endsection
